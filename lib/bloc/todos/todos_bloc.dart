53c2801654dbee06aa07fd1ead7ace450bef1b1f
evanezcent <pratamayo16@gmail.com>
2020-12-26 17:52:11 +0700

    bloc is done

--- lib/bloc/todos/todos_bloc.dart
+++ lib/bloc/todos/todos_bloc.dart
@@ -7,22 +7,84 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todos_repository/todos_repository.dart';
 
 class TodosBloc extends Bloc<TodosEvent, TodosState> {
-  final TodosRespository _todosRespository;
+  final TodosRespository _todosRepository;
   StreamSubscription _subscription;
 
   TodosBloc(TodosRespository todosRespository)
       : assert(todosRespository != null),
-        _todosRespository = todosRespository,
+        _todosRepository = todosRespository,
         super(TodosLoading());
 
   Stream<TodosState> _mapLoadtoState() async* {
     _subscription?.cancel();
     _subscription =
-        _todosRespository.todos().listen((data) => add(TodosUpdated(data)));
+        _todosRepository.todos().listen((data) => add(TodosUpdated(data)));
+  }
+
+  Stream<TodosState> _mapAddTodotoState(AddTodo event) async* {
+    _todosRepository.addNewTodo(event.todo);
+  }
+
+  Stream<TodosState> _mapUpdateTodoToState(UpdateTodo event) async* {
+    _todosRepository.updateTodo(event.updatedTodo);
+  }
+
+  Stream<TodosState> _mapDeleteTodoToState(DeleteTodo event) async* {
+    _todosRepository.deleteTodo(event.todo);
+  }
+
+  Stream<TodosState> _mapAllToggletoState() async* {
+    final currentState = state;
+    if (currentState is TodosLoaded) {
+      final allComplete =
+          currentState.todos.every((element) => element.complete);
+      final List<Todo> updatedTodos = currentState.todos
+          .map((todo) => todo.copyWith(complete: !allComplete))
+          .toList();
+
+      updatedTodos.forEach((element) {
+        _todosRepository.updateTodo(element);
+      });
+    }
+  }
+
+  Stream<TodosState> _mapClearCompletedToState() async* {
+    final currentState = state;
+    if (currentState is TodosLoaded) {
+      final List<Todo> completedTodos =
+          currentState.todos.where((todo) => todo.complete).toList();
+      completedTodos.forEach((completedTodo) {
+        _todosRepository.deleteTodo(completedTodo);
+      });
+    }
+  }
+
+  Stream<TodosState> _mapTodosUpdateToState(TodosUpdated event) async* {
+    yield TodosLoaded(event.todos);
+  }
+
+  @override
+  Future<void> close() {
+    _subscription?.cancel();
+    return super.close();
   }
 
   @override
   Stream<TodosState> mapEventToState(TodosEvent event) async* {
-    if (event is LoadTodos) {}
+    if (event is LoadTodos) {
+      yield* _mapLoadtoState();
+    } else if (event is AddTodo) {
+      yield* _mapAddTodotoState(event);
+    } else if (event is UpdateTodo) {
+      yield* _mapUpdateTodoToState(event);
+    } else if (event is DeleteTodo) {
+      yield* _mapDeleteTodoToState(event);
+    } else if (event is ToggleAll) {
+      yield* _mapAllToggletoState();
+    } else if (event is ClearCompleted) {
+      yield* _mapClearCompletedToState();
+    } else if (event is TodosUpdated) {
+      yield* _mapTodosUpdateToState(event);
+    }
   }
 }
